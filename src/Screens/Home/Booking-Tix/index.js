572d09d183d20ad95a2b4ea957a88650e08f36ef
dangtuan08 <48054696+dangtuan08@users.noreply.github.com>
2020-03-09 17:08:50 +0700

    add html trang dat ghe

--- src/Screens/Home/Booking-Tix/index.js
+++ src/Screens/Home/Booking-Tix/index.js
@@ -3,9 +3,242 @@ import React, { Component } from "react";
 export default class BookingTix extends Component {
   render() {
     return (
-      <>
-      <div>trang dat ve</div>
-       </>
+      <section className="booking">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-sm-12 col-md-8 col-lg-9 col-xl-8 booking-seat">
+              <div className="row">
+                <div className="col-9">
+                  <p>GXL - Nguyễn Du</p>
+                  <p>Thứ 6- 10:15 - Rạp 3</p>
+                </div>
+                <div className="col-3">
+                  <span>Thời gian giữ ghế: 4:00</span>
+                </div>
+                <div className="col-12 screen">
+                  <img
+                    className="img-fluid w-100"
+                    src={require('./../../../Assets/img/screen.png')}
+                    alt
+                  />
+                </div>
+                <div className="col-12 pb-5">
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat seat-vip mr-1 mb-1" />
+                    <span className="seat mr-1 mb-1"></span>
+                  </div>
+                  <div className="row list-seat py-5">
+                    <span className="seat mr-1 mb-1"></span>
+                    <span className="pr-5">Ghế thường</span>
+                    <span className="seat seat-vip mr-1 mb-1" />{" "}
+                    <span className="pr-5">Ghế VIP</span>
+                    <span className="seat seat-disable mr-1 mb-1" disable />
+                    <span className="pr-5">Ghế đã có người mua</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-4 booking-info">
+              <div className="text-center py-4">
+                <h1 className="money">0 VND</h1>
+              </div>
+              <hr />
+              <div className="booking-tiket-detail">
+                <p className="movie-name ">
+                  <span className="age px-3 py-1">P</span> Truy tìm phép thuật
+                </p>
+                <p className>GLX - Nguyễn Du</p>
+                <p className>
+                  <span>Thứ 6 06/03/2020 - 10:15 - RẠP 3</span>
+                </p>
+                <hr />
+                <p className="color-ghe py-3">
+                  Ghế: <span className="pr-1 color-ghe">F10</span>
+                  <span className="pr-1 color-ghe">F11</span>
+                </p>
+                <hr />
+              </div>
+              <div className="booking-btn-buy btn btn-success py-3">ĐẶT VÉ</div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
