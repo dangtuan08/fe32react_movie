21af646293744d43494be2c0d0eb21c1b791e532
dangtuan08 <48054696+dangtuan08@users.noreply.github.com>
2020-03-25 11:31:12 +0700

    sua giao dien login va navbar cua admin

--- src/Screens/Admin/LoginAdmin/index.js
+++ src/Screens/Admin/LoginAdmin/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as action from "../../../Store/Actions/User";
-
-
+import "./index.css";
 
 class Admin extends Component {
   constructor(props) {
@@ -25,36 +24,85 @@ class Admin extends Component {
     this.props.login(this.state, this.props.history);
   };
 
+  componentDidMount(){
+    if(localStorage.getItem("UserAdmin")){
+      this.props.history.push("/quan-ly-user")
+    }
+  }
   render() {
     return (
-        
-      <div className="container">
-        <div className="col-sm-6 mx-auto">
-          <form onSubmit={this.handleSumit}>
-            <div className="form-group">
-              <label htmlFor="">Username</label>
-              <input
-                type="text"
-                className="form-control"
-                name="taiKhoan"
-                onChange={this.handleOnchange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="">Password</label>
-              <input
-                type="text"
-                className="form-control"
-                name="matKhau"
-                onChange={this.handleOnchange}
-              />
-            </div>
-            <button type="submit" className="btn btn-success">
-              Login
+      <div className="login-form">
+        <form onSubmit={this.handleSumit}>
+          <div className="avatar">
+            <img src="./img/avatar.png" alt="Avatar" />
+          </div>
+          <h2 className="text-center">Member Login</h2>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              name="taiKhoan"
+              onChange={this.handleOnchange}
+              placeholder="Username"
+              required="required"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              name="matKhau"
+              placeholder="Password"
+              required="required"
+              onChange={this.handleOnchange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary btn-lg btn-block">
+              Sign in
             </button>
-          </form>
-        </div>
+          </div>
+          <div className="clearfix">
+            <label className="pull-left checkbox-inline">
+              <input type="checkbox" /> Remember me
+            </label>
+            <a href="#" className="pull-right">
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p className="text-center small">
+          Don't have an account? <a href="#">Sign up here!</a>
+        </p>
       </div>
+
+      // <div className="container">
+      //   <div className="col-sm-6 mx-auto">
+      //     <form onSubmit={this.handleSumit}>
+      //       <div className="form-group">
+      //         <label htmlFor="">Username</label>
+      //         <input
+      //           type="text"
+      //           className="form-control"
+      //           name="taiKhoan"
+      //           onChange={this.handleOnchange}
+      //         />
+      //       </div>
+      //       <div className="form-group">
+      //         <label htmlFor="">Password</label>
+      //         <input
+      //           type="text"
+      //           className="form-control"
+      //           name="matKhau"
+      //           onChange={this.handleOnchange}
+      //         />
+      //       </div>
+      //       <button type="submit" className="btn btn-success">
+      //         Login
+      //       </button>
+      //     </form>
+      //   </div>
+      // </div>
     );
   }
 }
